25a00ae19765601c6f3bba79198c648b794507c5
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-03-04 16:22:01 +0600

    OK

--- Supershop.cpp
+++ Supershop.cpp
@@ -30,6 +30,10 @@ void mobilebank();
 void visa();
 
 void clearcart();
+// search item;
+void search();
+// menu for search;
+void menu3(string &a);
 
 // exit;
 void exit()
@@ -44,10 +48,11 @@ int main()
 // first list;
 void firstlist()
 {
-    cout << "1--View Product list\n2--Buy Product\n3--Cart item\n4--exit\n:";
+    cout << "1--View Product list\n2-search product\n3--Buy Product\n4--Cart item\n5--exit\n:";
     int a;
     cin >> a;
     switch (a)
+
     {
     case 1:
         itemlist();
@@ -55,15 +60,21 @@ void firstlist()
 
         break;
     case 2:
-        itemlist();
-        selectitem();
+        search();
         break;
     case 3:
-        cart();
-        menu1();
+        itemlist();
+        selectitem();
+
         break;
     case 4:
+        cart();
+        menu1();
+
+        break;
+    case 5:
         exit();
+
         break;
     default:
         cout << "Wrong input\nselect again\n";
@@ -515,5 +526,226 @@ void visa()
     {
         cout << "Wrong Input\ntry again\n:";
         visa();
+    }
+}
+// search item;
+void search()
+{
+
+    vector<string> sitem;
+    sitem.push_back("beef");
+    sitem.push_back("chicken");
+    sitem.push_back("egg");
+    sitem.push_back("rice");
+    sitem.push_back("sweet");
+    sitem.push_back("mangoes");
+
+    cout << "Product Name :  ";
+    string str;
+    cin >> str;
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+
+    auto it = find(sitem.begin(), sitem.end(), str);
+    if (it != sitem.end())
+    {
+
+        cout << "This Product is Available\n";
+        menu3(str);
+    }
+    else
+    {
+        cout << "\nSorry this product Not Available at this Moment";
+        mainexit();
+    }
+}
+// menu for search;
+void menu3(string &a)
+{
+
+    cout << "1-Buy this Product\n2-Main menu\n3-exit\n:";
+    int b;
+    cin >> b;
+    if (b == 1)
+    {
+
+        if (a.compare("beef") == 0)
+        {
+            auto it = find(item.begin(), item.end(), a);
+
+            if (it != item.end())
+            {
+                int dis = distance(item.begin(), it);
+                int nq;
+                cout << "Quantity : ";
+                cin >> nq;
+                quantityy[dis] += nq;
+                int pr = nq * 500;
+                price[dis] += pr;
+            }
+            else
+            {
+                string s = "beef";
+                item.push_back(s);
+                cout << "Quantity : ";
+                int q;
+                cin >> q;
+                quantityy.push_back(q);
+                int p = q * 500;
+                price.push_back(p);
+            }
+        }
+        else if (a.compare("chicken") == 0)
+        {
+            string t = "chicken";
+            auto it = find(item.begin(), item.end(), t);
+
+            if (it != item.end())
+            {
+                int dis = distance(item.begin(), it);
+                int nq;
+                cout << "Quantity : ";
+                cin >> nq;
+                quantityy[dis] += nq;
+                int pr = nq * 300;
+                price[dis] += pr;
+            }
+            else
+            {
+                string s = "chicken";
+                item.push_back(s);
+                cout << "Quantity : ";
+                int q;
+                cin >> q;
+                quantityy.push_back(q);
+                int p = q * 300;
+                price.push_back(p);
+            }
+        }
+        else if (a.compare("egg") == 0)
+        {
+            string t = "egg";
+            auto it = find(item.begin(), item.end(), t);
+
+            if (it != item.end())
+            {
+                int dis = distance(item.begin(), it);
+                int nq;
+                cout << "Quantity : ";
+                cin >> nq;
+                quantityy[dis] += nq;
+                int pr = nq * 120;
+                price[dis] += pr;
+            }
+            else
+            {
+                string s = "egg";
+                item.push_back(s);
+                cout << "Quantity : ";
+                int q;
+                cin >> q;
+                quantityy.push_back(q);
+                int p = q * 120;
+                price.push_back(p);
+            }
+        }
+        else if (a.compare("rice") == 0)
+        {
+            string t = "rice";
+            auto it = find(item.begin(), item.end(), t);
+
+            if (it != item.end())
+            {
+                int dis = distance(item.begin(), it);
+                int nq;
+                cout << "Quantity : ";
+                cin >> nq;
+                quantityy[dis] += nq;
+                int pr = nq * 50;
+                price[dis] += pr;
+            }
+            else
+            {
+                string s = "rice";
+                item.push_back(s);
+                cout << "Quantity : ";
+                int q;
+                cin >> q;
+                quantityy.push_back(q);
+                int p = q * 50;
+                price.push_back(p);
+            }
+        }
+        else if (a.compare("sweet") == 0)
+        {
+            string t = "sweet";
+            auto it = find(item.begin(), item.end(), t);
+
+            if (it != item.end())
+            {
+                int dis = distance(item.begin(), it);
+                int nq;
+                cout << "Quantity : ";
+                cin >> nq;
+                quantityy[dis] += nq;
+                int pr = nq * 300;
+                price[dis] += pr;
+            }
+            else
+            {
+                string s = "sweet";
+                item.push_back(s);
+                cout << "Quantity : ";
+                int q;
+                cin >> q;
+                quantityy.push_back(q);
+                int p = q * 300;
+                price.push_back(p);
+            }
+        }
+        else if (a.compare("mangoes") == 0)
+        {
+            string t = "chicken";
+            auto it = find(item.begin(), item.end(), t);
+
+            if (it != item.end())
+            {
+                int dis = distance(item.begin(), it);
+                int nq;
+                cout << "Quantity : ";
+                cin >> nq;
+                quantityy[dis] += nq;
+                int pr = nq * 150;
+                price[dis] += pr;
+            }
+            else
+            {
+                string s = "chicken";
+                item.push_back(s);
+                cout << "Quantity : ";
+                int q;
+                cin >> q;
+                quantityy.push_back(q);
+                int p = q * 150;
+                price.push_back(p);
+            }
+        }
+
+        cout << "Successfully Add to cart\n";
+        menu2();
+    }
+    else if (b == 2)
+    {
+        firstlist();
+    }
+    else if (b == 3)
+    {
+        exit();
+    }
+    else
+
+    {
+        cout << "Wrong Input.\nPlz try again : \n";
+        string str = a;
+        menu3(str);
     }
 }
